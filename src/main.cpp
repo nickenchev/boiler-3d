117197c8b66611915869f3a45484f6c01098cc3d
Nick Enchev <nikolaenchev@gmail.com>
2019-07-12 13:46:29 -0400

    Playing with vulkan renderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <memory>
 #include <boiler.h>
-#include <video/openglrenderer.h>
+#include <video/vulkanrenderer.h>
 #include "samplepart.h"
 
 int main(int argc, char *argv[])
 {
-	Boiler::Engine::getInstance().initialize(std::make_unique<Boiler::OpenGLRenderer>(false), 1024, 768);
+	Boiler::Engine::getInstance().initialize(std::make_unique<Boiler::VulkanRenderer>(), 1024, 768);
 
-	auto part = std::make_shared<SamplePart>();
-	Boiler::Engine::getInstance().start(part);
+	//auto part = std::make_shared<SamplePart>();
+	//Boiler::Engine::getInstance().start(part);
 
 	return 0;
 }
