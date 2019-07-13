5927d408b2efbce41fbc075b3494eba73b01073d
Nick Enchev <nikolaenchev@gmail.com>
2019-07-13 10:27:59 -0400

    Vulkan support cont.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 #include <boiler.h>
 #include <video/vulkanrenderer.h>
 #include "samplepart.h"
+#include <video/sdlwindow.h>
 
 int main(int argc, char *argv[])
 {
 	Boiler::Engine::getInstance().initialize(std::make_unique<Boiler::VulkanRenderer>(), 1024, 768);
 
-	//auto part = std::make_shared<SamplePart>();
-	//Boiler::Engine::getInstance().start(part);
+	auto part = std::make_shared<SamplePart>();
+	Boiler::Engine::getInstance().start(part);
 
 	return 0;
 }
--- src/samplepart.cpp
+++ src/samplepart.cpp
@@ -60,18 +60,22 @@ void SamplePart::onStart()
 		{ -0.5f,  0.5f, -0.5f }
 	});
 
+	/*
 	object = ecs.newEntity();
 	auto renderComp = ecs.createComponent<RenderComponent>(object, engine.getRenderer().loadModel(vertData));
 	renderComp->colour = Colour::fromRGBA(252, 171, 20, 255);
 	auto renderPos = ecs.createComponent<PositionComponent>(object, Rect(0, 0, 0, 0));
 	renderPos->rotationAxis = glm::vec3(0, 1, 0);
+	*/
 }
 
 void SamplePart::update(double deltaTime)
 {
+	/*
 	Engine &engine = Engine::getInstance();
 	EntityComponentSystem &ecs = engine.getEcs();
 
 	PositionComponent &pos = ecs.getComponentStore().retrieve<PositionComponent>(object);
 	pos.rotationAngle += 45.0f * deltaTime;
+	*/
 }
