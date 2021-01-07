8da7b1ddc26db67c83af7013f26e68224b66ff81
Nick Enchev <nikolaenchev@gmail.com>
2021-01-07 16:08:07 -0500

    Engine main args

--- src/main.cpp
+++ src/main.cpp
@@ -12,58 +12,69 @@
 
 int main(int argc, char *argv[])
 {
+	bool showHelp = false;
 	bool enableDebug = false;
 	if (argc > 1)
 	{
-		if (std::strcmp(argv[1], "-debug") == 0)
+		if (std::strcmp(argv[1], "--help") == 0)
+		{
+			showHelp = true;
+		}
+		else if (std::strcmp(argv[1], "--debug") == 0)
 		{
 			enableDebug = true;
 		}
 	}
-	Boiler::Size initialSize(1280, 720);
-
-	// create an SDL window
-	SDL_Window *win = nullptr;
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) == 0)
+	if (showHelp)
 	{
-		SDL_WindowFlags winFlags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI |
-													 SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
-
-        win = SDL_CreateWindow("Boiler", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                               initialSize.width, initialSize.height, winFlags);
-		if (!win)
-		{
-			throw std::runtime_error("Unable to create main window");
-		}
+		std::cout << "--debug : Enabled debugging and validation\n" << std::endl;
 	}
 	else
-    {
-		throw std::runtime_error("Error Initializing SDL: " + std::string(SDL_GetError()));
-    }
-	SDL_SetRelativeMouseMode(SDL_TRUE);
+	{
+		Boiler::Size initialSize(1280, 720);
 
-	// Vulkan extensions required by SDL
-	unsigned int extCount = 0;
-	SDL_Vulkan_GetInstanceExtensions(win, &extCount, nullptr);
-	std::vector<const char *> extensions;
-	extensions.resize(extCount);
-	SDL_Vulkan_GetInstanceExtensions(win, &extCount, extensions.data());
+		// create an SDL window
+		SDL_Window *win = nullptr;
+		if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) == 0)
+		{
+			SDL_WindowFlags winFlags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI |
+														SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
 
-	// create the renderer and surface
-	auto renderer = std::make_unique<Boiler::Vulkan::VulkanRenderer>(extensions, enableDebug);
-	VkSurfaceKHR surface = VK_NULL_HANDLE;
-	SDL_Vulkan_CreateSurface(win, renderer->getVulkanInstance(), &surface);
-	renderer->setSurface(surface);
+			win = SDL_CreateWindow("Boiler", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+								initialSize.width, initialSize.height, winFlags);
+			if (!win)
+			{
+				throw std::runtime_error("Unable to create main window");
+			}
+		}
+		else
+		{
+			throw std::runtime_error("Error Initializing SDL: " + std::string(SDL_GetError()));
+		}
+		SDL_SetRelativeMouseMode(SDL_TRUE);
 
-	// setup the engine itself
-	static Boiler::Engine engine(renderer.get());
-	engine.initialize(initialSize);
+		// Vulkan extensions required by SDL
+		unsigned int extCount = 0;
+		SDL_Vulkan_GetInstanceExtensions(win, &extCount, nullptr);
+		std::vector<const char *> extensions;
+		extensions.resize(extCount);
+		SDL_Vulkan_GetInstanceExtensions(win, &extCount, extensions.data());
 
-	// create the part and start it
-	auto part = std::make_shared<SamplePart>(engine);
-	engine.start(part);
+		// create the renderer and surface
+		auto renderer = std::make_unique<Boiler::Vulkan::VulkanRenderer>(extensions, enableDebug);
+		VkSurfaceKHR surface = VK_NULL_HANDLE;
+		SDL_Vulkan_CreateSurface(win, renderer->getVulkanInstance(), &surface);
+		renderer->setSurface(surface);
 
-	SDL_DestroyWindow(win);
+		// setup the engine itself
+		static Boiler::Engine engine(renderer.get());
+		engine.initialize(initialSize);
 
+		// create the part and start it
+		auto part = std::make_shared<SamplePart>(engine);
+		engine.start(part);
+
+		SDL_DestroyWindow(win);
+	}
 	return 0;
 }
